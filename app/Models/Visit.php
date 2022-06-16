0079c41d378d880c906612a19a66474a4270cf93
Kasq09 <105741228+Kasq09@users.noreply.github.com>
2022-06-16 18:37:56 +0300

    procfile added

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -15,7 +15,7 @@ class Visit extends Model
     Public $sortable =['name', 'surname' ];
 
 
-    public function clients()
+    public function client()
     {
         return $this->belongsTo(Client::class);
     }
