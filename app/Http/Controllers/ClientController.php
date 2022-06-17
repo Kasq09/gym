c11b065094bb1be6e925c22258fb55b275db8d88
Kasq09 <105741228+Kasq09@users.noreply.github.com>
2022-06-17 11:09:12 +0300

    remove sortable

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -14,7 +14,7 @@ class ClientController extends Controller
                 where('name','like', '%'.$filter.'%')
                 ->orWhere('surname','like', '%'.$filter.'%')
                 ->orWhere('phone','like', '%'.$filter.'%')
-                ->sortable()->paginate(20),
+                ->paginate(20),
             "filter" => $filter
         ]);
     }
