a242fc30cf67f71fdda069b1b9bffcb058f65c84
Daniels Gabranovs <86777283+Lapegle@users.noreply.github.com>
2022-06-19 20:06:41 +0300

    change db config

--- config/database.php
+++ config/database.php
@@ -66,11 +66,12 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'postgres://kkqsqqsrrjtbcn:fc55856cf7940c402229bea916d7edbf73a4b58ddab45d874a337635ee045f9e@ec2-52-30-159-47.eu-west-1.compute.amazonaws.com:5432/d9efmkmgnb0680
+            '),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd9efmkmgnb0680'),
+            'username' => env('DB_USERNAME', 'kkqsqqsrrjtbcn'),
+            'password' => env('DB_PASSWORD', 'fc55856cf7940c402229bea916d7edbf73a4b58ddab45d874a337635ee045f9e'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
